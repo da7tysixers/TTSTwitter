e785250cc33fb8718df05eb93fa34ae8915ea711
Emmanuel Orubele <blontf@gmail.com>
2019-10-09 15:16:55 -0400

    Updates to: AuthorizationController UserService Registration.html feed.html login.html Tweet TweetRepository

--- TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/repository/TweetRepository.java
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/repository/TweetRepository.java
@@ -8,12 +8,11 @@ import org.springframework.stereotype.Repository;
 import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.model.User;
 
-
-
 @Repository
 public interface TweetRepository extends CrudRepository<Tweet, Long> {
-	    List<Tweet> findAllByOrderByCreatedAtDesc();
-	    List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
-	    List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
-	}
+	List<Tweet> findAllByUserOrderByCreatedAtDesc(User user);
+	List<Tweet> findByTags_PhraseOrderByCreatedAtDesc(String phrase);
+	List<Tweet> findAllByUserInOrderByCreatedAtDesc(List<User> users);
+	List<Tweet> findAllByOrderByCreatedAtDesc();
+}
 
